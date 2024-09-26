0c34bb31c85a119e2c6b9c1ccfe5e901e6ab2af0
koike <yoship2006@yahoo.co.jp>
2024-09-27 01:00:00 +0900

    初期コミット

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ ta_tran = int(arrival_coin * TAN07)
 
 if st.button("計算"):
     st.success(f"GGドル交換は {gg_tran_coin:,} 円")
-    st.success(f"取引手数料（{TAN07*100:} %） {ta_tran:,} 円")
+    st.success(f"取引手数料（{int(TAN07 * 100):} %） {ta_tran:,} 円")
 
 # if st.button("還元率を計算"):
 #     rtp = calculate_rtp(prize_pool, total_entry_fee)
