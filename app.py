43f88e360750825c88b5134a8fc63e555166f0fa
MotoKoikeKeiba <yoship2006@gmail.com>
2024-09-27 12:26:06 +0900

    初期コミット

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ gg_doll = gg_tran_coin / exchange_rate
 if st.button("計算"):
     st.success(f"GGドル交換は  {gg_tran_coin:,} 円")
     st.success(f"取引手数料（{int(TAN07 * 100):} %） {ta_tran:,} 円")
-    st.success(f"GGドル {gg_doll:,} ")
+    st.success(f"GGドル {gg_doll:,.2f} ")
     # st.success(f"1ドルのレート（円） {exchange_rate:,} ")
 
 # if st.button("還元率を計算"):
